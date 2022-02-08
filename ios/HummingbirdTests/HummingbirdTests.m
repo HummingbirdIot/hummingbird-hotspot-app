19014005daee447cfd801a0383d6c21576d1cfec
Ivan, Yang Chi <microivan@live.com>
2022-02-08 17:03:07 +0800

    Rename

--- ios/HummingbirdTests/HummingbirdTests.m
+++ ios/HummingbirdTests/HummingbirdTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface MakerAppTests : XCTestCase
+@interface HummingbirdTests : XCTestCase
 
 @end
 
-@implementation MakerAppTests
+@implementation HummingbirdTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
